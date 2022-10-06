6b85900bf24fe567a0d3c18ca92bf80bb01b93b0
Crypticaz <kyle.pancamo@gmail.com>
2022-10-06 17:34:19 -0500

    sidebar close and open functionality

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
-import MapContainer from './MapContainer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import MapContainer from "./MapContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+function Sidebar() {
+  const [sideBarValue, setSideBarValue] = useState(false);
+  const toggleSideBar = () => {
+    setSideBarValue(!sideBarValue);
+  };
+
+  return (
+      <div className="sidebar-wrapper">
+        {sideBarValue === false ? (
+        <button className="sidebar-button" onClick={toggleSideBar}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            width="48"
+            id="sidebar-button"
+          >
+            <path d="M20 44 0 24 20 4l2.8 2.85L5.65 24 22.8 41.15Z" />
+          </svg>
+        </button>
+        ) : null }
+        {sideBarValue === true ? (
+        <div class="sidebar-menu" id="sidebar-menu">
+          <button class="close-button" id="close-button" onClick={toggleSideBar}>
+            X
+          </button>
+          <div class="sidebar-data-wrapper">
+            <div class="sidebar-data">Some pre-plan data</div>
+            <div class="sidebar-data">Some more pre-plan data</div>
+            <div class="sidebar-data">Even more pre-plan data</div>
+            <div class="sidebar-data">
+              Here's a lot of pre-plan data. Here's a lot of pre-plan data. Here's
+              a lot of pre-plan data. Here's a lot of pre-plan data. Here's a lot
+              of pre-plan data. Here's a lot of pre-plan data. Here's a lot of
+              pre-plan data.
+            </div>
+          </div>
+        </div>
+        ) : null }
+      </div>
+      
+  );
+}
 
 function App() {
   return (
     <div>
-      <div class="sidebar-wrapper">
-
-        <div class="sidebar-button">
-          <svg xmlns="http://www.w3.org/2000/svg" 
-            height="48" 
-            width="48"
-            id="sidebar-button">
-            <path d="M20 44 0 24 20 4l2.8 2.85L5.65 24 22.8 41.15Z"/>
-          </svg>
-        </div>
-
-        <div class="sidebar-menu" id="sidebar-menu">
-          <div class="close-button" id="close-button">
-            X
-          </div>
-          <div class="sidebar-data-wrapper">
-            <div class="sidebar-data">
-              Some pre-plan data
-            </div>
-            <div class="sidebar-data">
-              Some more pre-plan data
-            </div>
-            <div class="sidebar-data">
-              Even more pre-plan data
-            </div>
-            <div class="sidebar-data">
-              Here's a lot of pre-plan data.
-              Here's a lot of pre-plan data.
-              Here's a lot of pre-plan data.
-              Here's a lot of pre-plan data.
-              Here's a lot of pre-plan data.
-              Here's a lot of pre-plan data.
-              Here's a lot of pre-plan data.
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <Sidebar/>
       <MapContainer />
-
     </div>
   );
 }
