06a129375bd7930df0326a8f98c579c04b2d19c2
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-10-11 17:23:49 -0500

    Basic backend setup for mysql db connection

--- server.js
+++ server.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+require('dotenv').config()
+
 const PORT = 5000;
+
+app.use(express.json());
+
+console.log(process.env.MYSQL_USERNAME)
+
+const db = mysql.createConnection({
+  user: process.env.MYSQL_USERNAME,
+  host: process.env.MYSQL_HOST_NAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+      console.log("\x1b[41m", "MySQL Connection Error:")
+      throw err;
+  }
+
+  console.log("Connected to MySQL server.");
+});
+
+app.get('/test', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+
+app.post('/api/dbtest', (req, res) => {
+  db.query('SELECT * FROM locations', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+app.post('/test/:id', (req, res) => {
+  const {id} = req.params;
+  const {name} = req.body;
+
+  res.send(`Hello ${name}! Your id is ${id}`);
+});
 
 // start express server on port 5000
 app.listen(5000, () => {
