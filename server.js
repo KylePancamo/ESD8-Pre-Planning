c54d25d1080a239aa063ae1b9e15a5f4c074ef1d
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-10-11 15:28:05 -0500

    remove unused middleware for now

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-const path = require('path');
-const cors = require('cors');
 const express = require('express');
-const app = express(); // create express app
-app.use(cors());
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-// add middlewares
-const root = require('path').join(__dirname, 'build');
-app.use(express.static(root));
-
-app.use('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+const app = express();
+const PORT = 5000;
 
 // start express server on port 5000
-app.listen(process.env.PORT || 5000, () => {
-  console.log('server started');
-});
+app.listen(5000, () => {
+  console.log(`server started on http://localhost:${PORT} `);
+})
