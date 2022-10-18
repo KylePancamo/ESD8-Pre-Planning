12579c314380180740a2730057d5e3f608c3afd4
Crypticaz <kyle.pancamo@gmail.com>
2022-10-17 21:53:31 -0500

    Update the popup window

--- src/components/Popup/PopupWindow.js
+++ src/components/Popup/PopupWindow.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 
 function PopupWindow(props) {
   return (
@@ -10,28 +14,38 @@ function PopupWindow(props) {
       centered
       show={props.show}
       onHide={props.onHide}
+      backdrop="static"
+      keyboard={false}
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {props.markerLoc.marker_name}
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter" className="m-2">Test</Modal.Title>
+        <Row className="ms-4">
+          <Col xs={12} md={8}>
+          🎁
+          </Col>
+        </Row>
       </Modal.Header>
       <Modal.Body>
-        <h4>Marker Id: </h4>
-        <p>
-            Marker Location:
-            <br />
-            {props.markerLoc.latitude}
-            <br />
-            {props.markerLoc.longitude}
-
-        </p>
-        <p>
-            Marker Icon: 🎁
-        </p>
+        <Container>
+          <Row>
+            <Col xs={12} md={4}>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicText">
+                  <Form.Label>Marrker Icon URL</Form.Label>
+                  <Form.Control type="email" placeholder="Enter URL" />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col xs={6} lg={8}>
+              <b>Marker Location: </b>
+              {props.markerLoc.latitude},{props.markerLoc.longitude}
+            </Col>
+          </Row>
+        </Container>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={props.onHide}>Save</Button>
       </Modal.Footer>
     </Modal>
   );
