083e7e65ffb95a101a9c9adfee92883f2c3a7aed
Yash Kapoor <91301568+yiy045@users.noreply.github.com>
2022-11-08 15:36:42 -0600

    Made the Legend into a Modal
    will change to an actual legend just wanted something "Legend"-ish on there

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,8 +1,82 @@
 import Axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
 
 function Legend(props) {
-    
+    const [edit, setEdit] = useState(false);
+    const [legend, setLegend] = useState(true);
+    const [legendItems, setLegendItems] = useState([]);
+    const [legendItemsLoaded, setLegendItemsLoaded] = useState(false);
+
+    const fetchLegendItems = () => { //will fetch the images from the database
+        Axios.get("http://localhost:5000/api/getLegendItems")
+            .then((response) => {
+                setLegendItems(response.data);
+                setLegendItemsLoaded(true);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    useEffect(() => {
+        fetchLegendItems();
+    }, []);
+
+    const handleLegend = () => {
+        setLegend(!legend);
+
+        if (legend) {
+            props.setLegendValue(false);
+        } else {
+            props.setLegendValue(true);
+        }
+
+
+    }
+
+    return (
+        <div className="legend">
+            <button className="btn btn-primary" onClick={() => { handleLegend() }}>Legend</button>
+            <Modal 
+                show={legend}
+                onHide={() => {
+                    setLegend(false);
+                    props.setLegendValue(false);
+                }}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Legend
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="legend-items">
+                        {legendItemsLoaded ? (
+                            legendItems.map((legendItem) => {
+                                return (
+                                    <div className="legend-item">
+                                        <img src={legendItem.icon_url} alt={legendItem.icon_name} />
+                                        <p>{legendItem.icon_name}</p>
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <div className = "legend-item">
+                                <p>Loading...</p>
+                            </div>
+                            
+                        )}
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
 }
 export default Legend;
