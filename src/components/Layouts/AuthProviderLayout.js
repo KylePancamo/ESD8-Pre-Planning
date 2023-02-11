4ac86fd39ebf0d94ef12a983a208b99eb892c0a4
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2023-02-10 20:55:35 -0600

    Foundation for errorElement when Awaiting

--- src/components/Layouts/AuthProviderLayout.js
+++ src/components/Layouts/AuthProviderLayout.js
@@ -2,6 +2,8 @@ import { AuthProvider } from "../../hooks/AuthProvider";
 import { Suspense } from "react";
 import { useLoaderData, useOutlet, Await } from "react-router-dom";
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import AwaitFailedLayout from "./AwaitFailedLayout";
+
 export const AuthProviderLayout = () => {
   const outlet = useOutlet();
   const { response } = useLoaderData();
@@ -13,7 +15,7 @@ export const AuthProviderLayout = () => {
       <Await 
         resolve={response}
         fallback={<ProgressBar animated now={100} />}
-        errorElement={<div>Error</div>}
+        errorElement={<AwaitFailedLayout/>}
       >
         {(resolvedUser) => <AuthProvider response = {resolvedUser}>{outlet}</AuthProvider>}
       </Await>
---
+++ src/components/Layouts/AwaitFailedLayout.js
@@ -0,0 +1,9 @@
+const AwaitFailedLayout = () => {
+    return (
+        <div className="await-failed-layout">
+            <h1>Something went wrong</h1>
+        </div>
+    );
+};
+
+export default AwaitFailedLayout;
