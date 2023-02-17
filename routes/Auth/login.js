61e10ccafea4e309d86912dc3e0615753c89f126
Crypticaz <kyle.pancamo@gmail.com>
2023-02-16 18:14:00 -0600

    Auth/login.js
    - update query for selecting new fields

--- routes/Auth/login.js
+++ routes/Auth/login.js
@@ -15,8 +15,16 @@ router.post("/", (req, res) => {
         return res.status(400).json({ message: "All fields are required" });
     }
 
+    const query = `SELECT a.id, a.username, r.name AS role_name, p.name AS permission_type, p.id AS permission
+                FROM accounts AS a
+                JOIN user_roles AS ur ON a.id = ur.user_id
+                JOIN roles AS r ON ur.role_id = r.id
+                JOIN role_permissions AS rp ON r.id = rp.role_id
+                JOIN permissions AS p ON rp.permission_id = p.id
+                WHERE a.username = ?`;
+
     db.query(
-        "SELECT password FROM accounts WHERE username = ?",
+        query,
         username,
         (err, result) => {
             if (err) {
@@ -25,7 +33,7 @@ router.post("/", (req, res) => {
             if (result.length > 0) {
                 //bcrypt.compare(password, result[0].password, (error, response) => {
                 //    if (response) {
-                        const token = jwt.sign({ username }, process.env.SECRET_KEY_JWT, { expiresIn: "24hr" });
+                        const token = jwt.sign(result[0], process.env.SECRET_KEY_JWT, { expiresIn: "24hr" });
                         res.cookie("token", token, { httpOnly: true });
                         res.send({ message: 'Logged in successfully', token } )
                 //    } else {
