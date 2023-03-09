1f0e7cba7f38108c17592b6c97dcb7e7abc73c31
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2023-03-09 10:55:24 -0600

    Convert RegisterUsers to tsx

--- src/pages/AdminPortal/RegisterUsers.tsx
+++ src/pages/AdminPortal/RegisterUsers.tsx
@@ -7,48 +7,47 @@ import Button from "react-bootstrap/Button";
 
 
 function RegisterUsers() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [registerStatus, setRegisterStatus] = useState(undefined);
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
 
 
     return (
         <div className="register-user-container">
             <h2>Register Users</h2>
             <Form className="register-user-form">
-                <Form.Group size="lg" controlId="username">
+                <Form.Group controlId="username">
                     <Form.Label style={{
                         fontFamily: 'Arial',
                     }}>Username (4 characters max)</Form.Label>
                     <Form.Control
                     //length of textbox
                         className="w-50"
-                        maxLength="4"
+                        maxLength={4}
                         type="username"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
                 </Form.Group>
-                <Form.Group size="lg" controlId="password">
+                <Form.Group controlId="password">
                     <Form.Label style={{
                         fontFamily: 'Arial',
                     }}>Password (8 characters max)</Form.Label>
                     <Form.Control
                         className="w-50"
-                        maxLength="8"
+                        maxLength={4}
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </Form.Group>
-                <Form.Group size="lg" controlId="confirmPassword">
+                <Form.Group controlId="confirmPassword">
                     <Form.Label style={{
                         fontFamily: 'Arial',
                     }}>Confirm Password</Form.Label>
                     <Form.Control
                         className="w-50"
-                        maxLength="8"
+                        maxLength={8}
                         type="password"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
