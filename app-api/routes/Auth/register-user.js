f08102b29099d9b80f9f7dab5bfee64e0a8cbd75
Yash Kapoor <91301568+yiy045@users.noreply.github.com>
2023-04-01 15:22:50 -0500

    Updated rehister-users.js
    Still need to work on it

--- app-api/routes/Auth/register-user.js
+++ app-api/routes/Auth/register-user.js
@@ -1,8 +1,29 @@
 const express = require("express");
-const router = express("router");
-const jwt = require("jsonwebtoken");
+const router = express.Router();
 
 const createDBConnection = require("../mysql");
-const {isAuthorized} = require('../middleware/authorization');
+const verifyUserCredentials = require("../middleware/verifyUserCredentials");
+const {isAdmin} = require("../middleware/authorization");
 
-router.post("/")
+router.post("/",verifyUserCredentials, isAdmin, (req, res) => { 
+    const db = createDBConnection("auth");
+
+    const u_name = req.body;
+    const p_word = req.body;
+
+    const query = `INSERT INTO users (username, password) VALUES (?, ?);`;
+
+    const data = [u_name, p_word];
+    db.query(query, data, (err, result) => {
+        if (err) {
+            res.send({ status: 'error', err: 'Error creating user.' });
+            return;
+        }
+
+        res.send({status: 'success'});
+    })
+
+});
+
+module.exports = router;
+
